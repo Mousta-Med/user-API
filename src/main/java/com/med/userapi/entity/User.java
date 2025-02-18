878edbcefc384e6b1cefeb7f45b6857cc4571030
Mousta-Med <moustamuhammed@gmail.com>
2025-02-18 23:19:57 +0100

    create User entity with all attributes

---
+++ src/main/java/com/med/userapi/entity/User.java
@@ -0,0 +1,32 @@
+package com.med.userapi.entity;
+
+import com.med.userapi.enums.Role;
+import jakarta.persistence.*;
+import lombok.Data;
+
+import java.time.LocalDate;
+import java.util.UUID;
+
+@Entity
+@Data
+public class User {
+    @Id
+    @GeneratedValue
+    private UUID id;
+    private String firstName;
+    private String lastName;
+    private LocalDate birthDate;
+    private String city;
+    private String country;
+    private String avatar;
+    private String company;
+    private String jobPosition;
+    private String mobile;
+    @Column(unique = true)
+    private String username;
+    @Column(unique = true)
+    private String email;
+    private String password;
+    @Enumerated(EnumType.STRING)
+    private Role role;
+}
---
+++ src/main/java/com/med/userapi/enums/Role.java
@@ -0,0 +1,6 @@
+package com.med.userapi.enums;
+
+public enum Role {
+    ADMIN,
+    USER
+}
